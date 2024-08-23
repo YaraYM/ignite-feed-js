4cdaae61c3a521cb3fb334d1acd1acee907f3087
YaraYM <yaraym0408@gmail.com>
2024-08-23 08:43:18 -0300

    Criação do componente Header

--- ignite-feed-js/src/App.jsx
+++ ignite-feed-js/src/App.jsx
@@ -1,14 +1,20 @@
-import { Post } from './Post'
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './styles.css'
 
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Header />
+      <Post 
+        author="Tony Corleone" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
+      />
+      <Post 
+        author="Ipa Corleone" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
+      />
     </>
   )
 }
