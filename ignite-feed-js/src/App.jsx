a3fc88c4f39158148428801a276fff2431715147
YaraYM <yaraym0408@gmail.com>
2024-08-26 13:04:55 -0300

    Sidebar Component

--- ignite-feed-js/src/App.jsx
+++ ignite-feed-js/src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
+
 
 export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author="Tony Corleone" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
-      />
-      <Post 
-        author="Ipa Corleone" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Tony Corleone" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
+          />
+          <Post 
+            author="Ipa Corleone" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deserunt dolorum, enim inventore ea earum voluptates vitae atque iste facilis consequatur nisi illum quasi quibusdam voluptas doloribus mollitia totam natus." 
+          />
+        </main>
+      </div>
     </>
   )
 }
