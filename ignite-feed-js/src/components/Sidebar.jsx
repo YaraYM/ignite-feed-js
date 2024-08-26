315133696d9041a40c71b101116feb5bd20f9cd2
YaraYM <yaraym0408@gmail.com>
2024-08-26 13:33:18 -0300

    finalizando Sidebar

--- ignite-feed-js/src/components/Sidebar.jsx
+++ ignite-feed-js/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/YaraYM.png" />
+
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={24} />
           Editar seu perfil
         </a>
       </footer>
