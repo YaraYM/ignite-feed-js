bbce9c23f0f8cba70e04234dd0a08874772a0d5d
YaraYM <yaraym0408@gmail.com>
2024-08-27 08:29:34 -0300

    Estilizações do Post

--- ignite-feed-js/src/components/Post.jsx
+++ ignite-feed-js/src/components/Post.jsx
@@ -18,8 +18,12 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa ??</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare ??</p>
-        <p>?? <a href=''>jane.design/doctorcare</a></p>
-        <p><a href=''>#novoprojeto</a> <a href=''>#nlw</a> <a href=''>#rocketseat</a></p>
+        <p><a href=''>jane.design/doctorcare</a></p>
+        <p>
+          <a href=''>#novoprojeto</a>{' '}
+          <a href=''>#nlw</a>{' '}
+          <a href=''>#rocketseat</a>{' '}
+        </p>
       </div>
     </article>
   )
