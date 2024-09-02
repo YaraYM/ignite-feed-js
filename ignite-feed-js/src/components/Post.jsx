0a2b87dd406722d1b658ae93d7b6ac90eb6c32e7
YaraYM <yaraym0408@gmail.com>
2024-09-02 13:25:04 -0300

    Inserindo comentários

--- ignite-feed-js/src/components/Post.jsx
+++ ignite-feed-js/src/components/Post.jsx
@@ -11,9 +11,10 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1, 
-    2,
+    'Post muito bacana, hein?!'
   ])
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -27,7 +28,13 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment(event) {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -60,7 +67,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea 
+          name='comment'
           placeholder='Deixe um coment�rio'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -70,7 +80,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
