91d8f04ccdc583bdb5077569f5dec3fa52478594
Yara Yamanaka Marques <yaraym0408@gmail.com>
2024-09-02 00:06:17 -0300

    Propriedades Post

--- ignite-feed-js/src/components/Post.jsx
+++ ignite-feed-js/src/components/Post.jsx
@@ -6,7 +6,7 @@ import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-export function Post({ author, publishedAt }) {
+export function Post({ author, publishedAt, content }) {
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
   });
@@ -33,14 +33,13 @@ export function Post({ author, publishedAt }) {
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa ??</p>
-        <p>Acabei de subir mais um projeto no meu portifa. � um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto � DoctorCare ??</p>
-        <p><a href=''>jane.design/doctorcare</a></p>
-        <p>
-          <a href=''>#novoprojeto</a>{' '}
-          <a href=''>#nlw</a>{' '}
-          <a href=''>#rocketseat</a>{' '}
-        </p>
+        {content.map(line => {
+          if(line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if(line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
