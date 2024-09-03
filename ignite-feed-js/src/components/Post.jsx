428bf8c0a5a756241f76c759ebb31549186101be
YaraYM <yaraym0408@gmail.com>
2024-09-03 13:21:45 -0300

    Validação formulário

--- ignite-feed-js/src/components/Post.jsx
+++ ignite-feed-js/src/components/Post.jsx
@@ -32,11 +32,15 @@ export function Post({ author, publishedAt, content }) {
 
     setComments([...comments, newCommentText]);
     setNewCommentText('');
-
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório');
   }
 
   function deleteComment(commentToDelete) {
@@ -48,6 +52,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -83,10 +89,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder='Deixe um comentï¿½rio'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
